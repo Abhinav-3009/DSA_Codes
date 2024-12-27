62a8fe3acdf39ae33ce37af085ddf447d1a31e72
Abhinav Gupta <abhinav30g@gmail.com>
2024-12-27 23:52:56 +0530

    Time: 0 ms (100%), Space: 47.5 MB (20.82%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int n = values.size();
-        if (n < 2) return values[0] + values[1] + 0 - 1; 
-        vector<int> suffixMax(n);
-        suffixMax[n - 1] = values[n - 1] - (n - 1);
-        for (int i = n - 2; i >= 0; i--) {
-            suffixMax[i] = max(suffixMax[i + 1], values[i] - i);
+        if(n<2){
+            return values[0] + values[1] - 1;
         }
-        int maxScore = INT_MIN;
-        for (int i = 0; i < n - 1; i++) {
-            maxScore = max(maxScore, values[i] + i + suffixMax[i + 1]);
+        vector<int> suf(n);
+        suf[n-1] = values[n-1] - (n-1);
+        for(int j = n-2; j>=0; j--){
+            suf[j] = max(suf[j+1], values[j] - j);
+        }
+        int ans = INT_MIN;
+        for(int i = 0; i < n-1; i++){
+            ans = max(ans, values[i] + i + suf[i+1]);
         }
 
-        return maxScore;
+        return ans;
     }
 };
